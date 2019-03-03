97a045d816ed8389ecfefd7421367bf131594a1c
Hamza Çakmak <reelaktor@gmail.com>
2019-03-03 14:21:34 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _155.gün_switch_case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Yılın 5. Günü salı ise 155. gün hangisidir...?
+            int k = 155 % 7;
+            switch(k)
+            {
+                case 0: Console.WriteLine("Perşembe"); break;
+                case 1: Console.WriteLine("cuma"); break;
+                case 2: Console.WriteLine("Cumartesi"); break;
+                case 3: Console.WriteLine("pazar"); break;
+                case 4: Console.WriteLine("pazartesi"); break;
+                case 5: Console.WriteLine("salı"); break;
+                case 6: Console.WriteLine("Çarşamba"); break;
+            }
+        }
+    }
+}
